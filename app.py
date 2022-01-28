e5f39f700fc53ae17b8bbe6d32e2ca4aae8f50cc
Jonathah Liu <jonathan.tzuchi.liu@gmail.com>
2022-01-28 21:11:55 +0800

    closing flask's debug mode

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def update_result():
     return {'pos1': output[0], 'pos2': output[1], 'pos3': output[2]}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
 #this tells how to update text without refreshing
 # https://blog.miguelgrinberg.com/post/dynamically-update-your-flask-web-pages-using-turbo-flask
