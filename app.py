e4a70ccae0a0907b1537c18ae5cee523e3f96428
Jonathah Liu <jonathan.tzuchi.liu@gmail.com>
2022-02-19 13:35:52 +0800

    0219 backup of the website

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+
 async_mode = None
+
 app = Flask(__name__)
-socket_ = SocketIO(app, async_mode=async_mode)
-
-@app.route('/')
-def index():
-    return render_template('website.htm',
-                           sync_mode=socket_.async_mode)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+socketio = SocketIO(app)
 
 
-if __name__ == '__main__':
-    socket_.run(app, debug=True)
+@socketio.on("image")
+def process(msg):
+    print(msg)
+    # do image processing thing
+    emit("answer", 'Server received message "%s" at %s' % (msg, datetime.now().strftime("%H:%M:%S")))
+
+
+@app.route("/")
+def home():
+    return render_template('website.html', async_mode=socketio.async_mode)
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=5002)
